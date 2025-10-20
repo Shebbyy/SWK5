3272045ea7775246b5f095f6e09014f2fd114c9a
Sebastian Gradwohl <s2310307057@students.fh-hagenberg.at>
2025-10-20 09:06:07 +0200

    Seminar 2: - Created 2 Projects for the HashDictionary Library and the Testing Client

---
+++ 02/HashDictionary/HashDictionary.Impl/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace HashDictionary.Impl;
+
+// Target Framework .netstandard = below Core/Xamarin/Full Framework; Whenever a Lib needs to be compatible with different 
+// Project Types
+public class Class1 {
+}
