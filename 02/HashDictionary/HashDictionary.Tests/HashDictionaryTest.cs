3bccbf5cd1a41a345f241cfc0efdce0f58057c5c
Sebastian Gradwohl <s2310307057@students.fh-hagenberg.at>
2025-10-20 11:57:24 +0200

    Seminar 2: - Added UnitTests

---
+++ 02/HashDictionary/HashDictionary.Tests/HashDictionaryTest.cs
@@ -0,0 +1,31 @@
+using HashDictionary.Impl;
+
+namespace HashDictionary.Tests;
+
+public class HashDictionaryTest {
+    [Fact]
+    public void AddAndIndexerGetterConsistent() {
+        var dict = new HashDictionary<int, int>();
+
+        dict.Add(1, 10);
+        dict[2] = 22;
+
+        Assert.Equal(10, dict[1]);
+        Assert.Equal(22, dict[2]);
+    }
+
+    [Theory]
+    [InlineData(new[] {10, 20}, 2)]
+    [InlineData(new[] {10, 20, 30}, 3)]
+    [InlineData(new int[] {}, 0)]
+    public void Theory_CountPropertyIsOk_WhenAddingItems(IEnumerable<int> list, int expected) {
+        var dict = new HashDictionary<int, int>();
+        int i = 0;
+        foreach (var entry in list) {
+            dict.Add(i, entry);
+            i++;
+        }
+        
+        Assert.Equal(expected, dict.Count);
+    }
+}
