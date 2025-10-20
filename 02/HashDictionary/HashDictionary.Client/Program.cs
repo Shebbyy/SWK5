6c28bca262b5767f2d9977495acd34bce8fba45c
Sebastian Gradwohl <s2310307057@students.fh-hagenberg.at>
2025-10-20 10:50:21 +0200

    Seminar 2: - Implemented essential Logic for the HashDictionary and built a small validation function in HashDirectory.Client

--- 02/HashDictionary/HashDictionary.Client/Program.cs
+++ 02/HashDictionary/HashDictionary.Client/Program.cs
@@ -1,3 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Implicit Usings Project Setting can be used to reduce the amount of imports/usings that are needed to run a prg 
-Console.WriteLine("Hello, World!");
+
+using HashDictionary.Impl;
+
+IDictionary<String, int> TestIndexerAndAdd() {
+    var cityInfo = new HashDictionary<String, int>();
+
+    cityInfo["Hagenberg"] = 2_100;
+    cityInfo["Linz"]      = 100_000;
+    cityInfo["Linz"]      = 200_000;
+    cityInfo.Add("Wien", 1_700_000);
+    try {
+        cityInfo.Add("Wien", 1_000_000);
+    }
+    catch (ArgumentException e) {
+        Console.WriteLine(e.GetType().Name + e.Message);
+    } // throws ArgumentException
+    
+    Console.WriteLine($"dict[\"Hagenberg\"] = {cityInfo["Hagenberg"]}");
+    Console.WriteLine($"dict[\"Linz\"]      = {cityInfo["Linz"]}");
+    Console.WriteLine($"dict[\"Wien\"]      = {cityInfo["Wien"]}");
+    try {
+        Console.WriteLine($"dict[\"Graz\"]      = {cityInfo["Graz"]}"); // does not exist
+    }
+    catch (KeyNotFoundException e) {
+        Console.WriteLine(e.GetType().Name + e.Message);
+    }
+
+    return cityInfo;
+}
+
+TestIndexerAndAdd();
--- 02/HashDictionary/HashDictionary.Impl/HashDictionary.cs
+++ 02/HashDictionary/HashDictionary.Impl/HashDictionary.cs
@@ -1,8 +1,59 @@
 using System.Collections;
+using System.Diagnostics.CodeAnalysis;
 
 namespace HashDictionary.Impl;
 
 public class HashDictionary<TK, TV> : IDictionary<TK, TV> {
+    #region Inner Class Node
+        private class Node {
+            public required TK Key { get; set; }
+            public required TV Value { get; set; }
+            public Node Next { get; set; }
+        }
+    #endregion
+    
+    #region constants and private properties
+        private const int INITIAL_HASH_TABLE_SIZE = 8;
+        private Node[] ht = new Node[INITIAL_HASH_TABLE_SIZE];
+        private int IndexFor(TK key) => Math.Abs(key.GetHashCode() % ht.Length);
+    #endregion
+    
+    #region helper methods
+        private Node FindNode(TK key) {
+            Node n = ht[IndexFor(key)];
+    
+            // is not is safe from operator overloading, instead of !=
+            for (; n is not null; n = n.Next) {
+                if (Comparer.Equals(n.Key, key)) return n;
+            }
+    
+            return null;
+        }
+    
+        // Slightly more efficient than n.Key.Equals(), but only veeeery slightly
+        private static readonly EqualityComparer<TK> Comparer = EqualityComparer<TK>.Default;
+
+        private bool TryAdd(TK key, TV val, out Node node) {
+            node = FindNode(key);
+            if (node is not null) return false;
+
+            int idx = IndexFor(key);
+            node = ht[idx] = new Node {
+                Key = key, 
+                Value = val, 
+                Next = ht[idx]
+            };
+            Count++;
+
+            return true;
+        }
+    #endregion
+    
+    public ICollection<TK> Keys { get; }
+    public ICollection<TV> Values { get; }
+    public int Count { get; private set; }
+    public bool IsReadOnly => false;
+
     public IEnumerator<KeyValuePair<TK, TV>> GetEnumerator() {
         throw new NotImplementedException();
     }
@@ -11,17 +62,37 @@ public class HashDictionary<TK, TV> : IDictionary<TK, TV> {
         return GetEnumerator();
     }
 
-    public void Add(KeyValuePair<TK, TV> item) {
-        throw new NotImplementedException();
+    public void Add(KeyValuePair<TK, TV> item) => Add(item.Key, item.Value);
+    
+    public void Add(TK key, TV value) {
+        if (!TryAdd(key, value, out _)) {
+            throw new ArgumentException("Key already exists");
+        }
+    }
+    
+    public TV this[TK key] {
+        get {
+            Node n = FindNode(key);
+            return n is null ? throw new KeyNotFoundException() : n.Value;
+        }
+        
+        set {
+            // Either add in TryAdd, else update value
+            if (!TryAdd(key, value, out Node node)) {
+                node.Value = value;
+            }
+        }
+    }
+    
+    public bool TryGetValue(TK key, [MaybeNullWhen(false)] out TV value) {
+        Node n = FindNode(key);
+        value = n is not null ? n.Value : default;
+        return n is not null;
     }
 
-    public void Clear() {
-        throw new NotImplementedException();
-    }
-
-    public bool Contains(KeyValuePair<TK, TV> item) {
-        throw new NotImplementedException();
-    }
+    public bool Contains(KeyValuePair<TK, TV> item) => ContainsKey(item.Key);
+    public bool ContainsKey(TK key) => FindNode(key) is not null;
+    
 
     public void CopyTo(KeyValuePair<TK, TV>[] array, int arrayIndex) {
         throw new NotImplementedException();
@@ -30,30 +101,13 @@ public class HashDictionary<TK, TV> : IDictionary<TK, TV> {
     public bool Remove(KeyValuePair<TK, TV> item) {
         throw new NotImplementedException();
     }
-    
-    public void Add(TK key, TV value) {
-        throw new NotImplementedException();
-    }
-
-    public bool ContainsKey(TK key) {
-        throw new NotImplementedException();
-    }
 
     public bool Remove(TK key) {
         throw new NotImplementedException();
     }
-
-    public bool TryGetValue(TK key, out TV value) {
-        throw new NotImplementedException();
-    }
-
-    public TV this[TK key] {
-        get => throw new NotImplementedException();
-        set => throw new NotImplementedException();
-    }
     
-    public ICollection<TK> Keys { get; }
-    public ICollection<TV> Values { get; }
-    public int Count { get; }
-    public bool IsReadOnly { get; }
+    public void Clear() {
+        ht = new Node[INITIAL_HASH_TABLE_SIZE];
+        Count = 0;
+    }
 }
