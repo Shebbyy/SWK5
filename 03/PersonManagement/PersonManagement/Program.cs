36338a8eee7d42b1b2761306b8de7c8f5afc54ef
Sebastian Gradwohl <s2310307057@students.fh-hagenberg.at>
2025-10-22 10:40:05 +0200

    Seminar 3: - Added provided Solution as a basis

---
+++ 03/PersonManagement/PersonManagement/Program.cs
@@ -0,0 +1,57 @@
+﻿using PersonManagement;
+
+PersonRepository personRepository = new PersonRepository();
+IEnumerable<Person>? persons = new List<Person>();
+
+try
+{
+	//
+	// TODO
+	//
+}
+catch (FileNotFoundException fnfEx)
+{
+	Console.WriteLine(fnfEx.Message);
+	return;
+}
+
+// TODO
+
+//textWriter.WriteLine("=====================================================");
+//textWriter.WriteLine("Person list");
+//textWriter.WriteLine("=====================================================");
+//
+// TODO
+//
+
+//textWriter.WriteLine();
+//textWriter.WriteLine("=====================================================");
+//textWriter.WriteLine("Persons in Hagenberg");
+//textWriter.WriteLine("=====================================================");
+//
+// TODO
+//
+
+//textWriter.WriteLine();
+//textWriter.WriteLine("=====================================================");
+//textWriter.WriteLine("Person names");
+//textWriter.WriteLine("=====================================================");
+//
+// TODO
+//
+
+//textWriter.WriteLine();
+//textWriter.WriteLine("=====================================================");
+//textWriter.WriteLine($"Youngest person");
+//textWriter.WriteLine("=====================================================");
+//
+// TODO
+//
+
+//textWriter.WriteLine();
+//textWriter.WriteLine("=====================================================");
+//textWriter.WriteLine("Persons sorted by age ascending");
+//textWriter.WriteLine("=====================================================");
+//
+// TODO
+//
