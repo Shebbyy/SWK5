6bc08fe92b5d10c950c22c719d776bcd23675d45
Sebastian Gradwohl <s2310307057@students.fh-hagenberg.at>
2025-10-29 11:58:21 +0100

    Seminar 4: - Worked through NullableReferenceTypes

--- 04/NullableReferenceTypes/Program.cs
+++ 04/NullableReferenceTypes/Program.cs
@@ -1,20 +1,28 @@
-﻿var person = new Person("Huber", "Franz");
+﻿// enable for file:
+#nullable enable
+using System.Diagnostics.CodeAnalysis;
+
+// enable for project in project settings
+// It is just for the compiler, during runtime it is still possible to set null to a string, which would  throw an error during compilation
+
+var person = new Person("Huber", "Franz");
+// every reference datatype can be null, NullableReferenceTypes  has the goal to fix this in hindsight, very common error, can break old programs, therefore must be activated explicitly
 person.FirstName = null;
 person.LastName = null;
 person.LastName = "Huber-Mayr";
 
-var firstUpper = person.FirstName.ToUpper();
+var firstUpper = person.FirstName?.ToUpper();
 var lastUpper = person.LastName.ToUpper();
 
-IEnumerable<Person> persons = GetPersons();
-PrintPersons(persons);
+IEnumerable<Person>? persons = GetPersons();
+PrintPersons(persons ?? []);
 
-if (TryGetPerson(persons, "Huber", out Person p))
+if (TryGetPerson(persons, "Huber", out Person? p))
 {
     Console.WriteLine(p.LastName);
 }
 
-static IEnumerable<Person> GetPersons()
+static IEnumerable<Person>? GetPersons()
 {
     // returns null for whatever reason
     return null;
@@ -28,7 +36,7 @@ static void PrintPersons(IEnumerable<Person> persons)
     }
 }
 
-static bool TryGetPerson(IEnumerable<Person> persons, string lastName, out Person person)
+static bool TryGetPerson(IEnumerable<Person>? persons, string lastName, [NotNullWhen(true)]out Person? person)
 {
     if (persons is not null)
     {
@@ -46,9 +54,9 @@ static bool TryGetPerson(IEnumerable<Person> persons, string lastName, out Perso
     return false;
 }
 
-public class Person(string lastName, string firstName = null)
+public class Person(string lastName, string? firstName = null)
 {
-    public string FirstName { get; set; } = firstName;
+    public string? FirstName { get; set; } = firstName; // without nullablereference, can be null, with it active it needs to be set explicitly with string?
 
     public string LastName { get; set; } = lastName ?? throw new ArgumentNullException(nameof(lastName));
 }
