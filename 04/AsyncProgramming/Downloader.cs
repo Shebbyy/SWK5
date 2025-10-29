e1d9c38d33d9463a48c38f5ced83c0a4cb05ce8e
Sebastian Gradwohl <s2310307057@students.fh-hagenberg.at>
2025-10-29 09:07:54 +0100

    Checkin given Code from the instructor

---
+++ 04/AsyncProgramming/Downloader.cs
@@ -0,0 +1,43 @@
+﻿using static System.Console;
+
+public class Downloader
+{
+    public void DownloadSync(string url, string filePath)
+    {
+        // TODO
+        WriteLine($"{nameof(DownloadSync)}: Downloaded '{url}'");
+
+        // TODO
+        WriteLine($"{nameof(DownloadSync)}: Saved '{filePath}'");
+    }
+
+    public void DownloadAsync_Task(string url, string filePath)
+    {
+        // TODO
+        WriteLine($"{nameof(DownloadAsync_Task)}: Downloaded '{url}'");
+
+        // TODO
+        WriteLine($"{nameof(DownloadAsync_Task)}: Saved '{filePath}'");
+    }
+
+    public void DownloadAsync_Await(string url, string filePath)
+    {
+        // TODO
+        WriteLine($"{nameof(DownloadAsync_Await)}: Downloaded '{url}'");
+
+        // TODO
+        WriteLine($"{nameof(DownloadAsync_Await)}: Saved '{filePath}'");
+    }
+
+    public void DownloadMultipleAsync(string url1, string filePath1, string url2, string filePath2)
+    {
+        // TODO
+        WriteLine($"{nameof(DownloadMultipleAsync)}: {nameof(DownloadAsync_Await)} of '{url1}' started");
+
+        // TODO
+        WriteLine($"{nameof(DownloadMultipleAsync)}: {nameof(DownloadAsync_Await)} of '{url2}' started");
+
+        // TODO
+        WriteLine($"{nameof(DownloadMultipleAsync)}: {nameof(DownloadAsync_Await)} of all files completed");
+    }
+}
---
+++ 04/AsyncProgramming/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+
+const string URL1 = "https://github.com/progit/progit2/releases/download/2.1.360/progit.pdf";
+const string URL2 = "https://github.com/SoftUni/Programming-Basics-Book-CSharp-EN/raw/b153f06082ede1d0e4c8e7994c4d6a628886c22e/resources/Programming-Basics-CSharp-Book-and-Video-Lessons-Nakov-v2019.pdf";
+
+var downloader = new Downloader();
+
+WriteLine($"====================== {nameof(Downloader.DownloadSync)} ======================");
+// TODO
+WriteLine($"{nameof(Downloader.DownloadSync)} gave control back to caller");
+WriteLine($"{nameof(Downloader.DownloadSync)} completed work");
+WriteLine();
+
+WriteLine($"====================== {nameof(Downloader.DownloadAsync_Task)} ======================");
+// TODO
+WriteLine($"{nameof(Downloader.DownloadAsync_Task)} gave control back to caller");
+// TODO
+WriteLine($"{nameof(Downloader.DownloadAsync_Task)} completed work");
+WriteLine();
+
+WriteLine($"====================== {nameof(Downloader.DownloadAsync_Await)} ======================");
+// TODO
+WriteLine($"{nameof(Downloader.DownloadAsync_Await)} gave control back to caller");
+// TODO
+WriteLine($"{nameof(Downloader.DownloadAsync_Await)} completed work");
+WriteLine();
+
+WriteLine($"======================= {nameof(Downloader.DownloadMultipleAsync)} =======================");
+// TODO
+WriteLine($"{nameof(Downloader.DownloadMultipleAsync)} gave control back to caller");
+// TODO
+WriteLine($"{nameof(Downloader.DownloadMultipleAsync)} completed work");
+WriteLine();
+
+ReadLine(); 
