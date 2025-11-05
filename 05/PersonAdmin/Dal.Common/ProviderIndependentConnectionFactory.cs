f1bbb6c9731a3828fbbd16a34e13842ce5f231a5
Sebastian Gradwohl <s2310307057@students.fh-hagenberg.at>
2025-11-05 09:19:05 +0100

    Started Seminar 5: - Initial Setup

---
+++ 05/PersonAdmin/Dal.Common/ProviderIndependentConnectionFactory.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Common;
+
+namespace Dal.Common;
+
+public class ProviderIndependentConnectionFactory
+{
+    private readonly DbProviderFactory dbProviderFactory;
+    private readonly string connectionString;
+
+    public ProviderIndependentConnectionFactory(string providerName, string connectionString)
+    {
+        DbProviderFactories.RegisterFactory("Microsoft.Data.SqlClient", Microsoft.Data.SqlClient.SqlClientFactory.Instance);
+        DbProviderFactories.RegisterFactory("MySqlConnector", MySqlConnector.MySqlConnectorFactory.Instance); ;
+
+        this.dbProviderFactory = DbProviderFactories.GetFactory(providerName);
+        this.connectionString = connectionString;
+    }
+}
